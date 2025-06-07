fb6e87e5a066d285e67a4d96da6e02354ec9baeb
qiekn <qieknxxx@gmail.com>
2025-06-07 23:53:47 +0800

    feat: displaying player's hand

--- main.lua
+++ main.lua
@@ -20,3 +20,14 @@ function love.load()
 
   print("total number of cards in deck: " .. #deck)
 end
+
+function love.draw()
+  local output = {}
+
+  table.insert(output, "Player Hand:")
+  for cardIndex, card in ipairs(playerHand) do
+    table.insert(output, "suit: " .. card.suit .. ", rank: " .. card.rank)
+  end
+
+  love.graphics.print(table.concat(output, "\n"), 15, 15)
+end
